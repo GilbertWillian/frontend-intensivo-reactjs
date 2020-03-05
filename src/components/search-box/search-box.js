09ff5f47b6055fccb27596b3d35f7dbea1d7b437
GilbertWillian <gilbertowillian99@gmail.com>
2020-03-05 17:18:55 -0300

    First commit

---
+++ src/components/search-box/search-box.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import './search-box.css'
+
+function SearchBox() {
+  return (
+
+    <div>
+      <div className="container search-box">
+        <form className="flex">
+          <input className="form-control form-control-lg" placeholder="Qual o Pokémon?" />
+          <button className="btn btn-secondary btn-lg" type="submit">Search</button>
+        </form>
+      </div>
+
+      <div className="bg-light">
+        <div className="container">
+          <div className="results-box">
+
+            {/* Card */}
+            <div className="card text-white bg-card mb-3 cards">{/*style="max-width: 18rem;*/}
+              <div className="card-header">Nome do Pokémon</div>
+
+              <div className="card-body">
+
+                <h5 className="card-title">Tipos</h5>
+                <div className="types">
+                  <div className="type-poke">
+                    <p>Fogo</p>
+                  </div>
+                  <div className="type-poke">
+                    <p>Fogo</p>
+                  </div>
+                </div>
+
+                <h5 className="card-title">Fraquezas</h5>
+                <div className="types">
+                  <div className="type-poke">
+                    <p>Terra</p>
+                  </div>
+                  <div className="type-poke">
+                    <p>Agua</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SearchBox
