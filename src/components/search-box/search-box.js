d764092f7dd1f6b51849b209de960d308496d6e8
GilbertWillian <gilbertowillian99@gmail.com>
2020-03-09 12:29:34 -0300

    Atualizacoes parte 1 do dia 09/03/20

--- src/components/search-box/search-box.js
+++ src/components/search-box/search-box.js
@@ -1,66 +1,68 @@
 import React from 'react'
+import { isEmpty } from 'lodash'
 import './search-box.css'
 import PokeDexService from '../../service/pokedex.service'
 
 class SearchBox extends React.Component {
-  
+
   constructor(props) {
     super(props)
 
-    this.state = {}
+    this.state = {
+      pokemon: {}
+    }
   }
 
   componentDidMount() {
     PokeDexService.getPokeInfo()
-    .then((response) => {
-      this.setState({
-        pokemon: response
+      .then((response) => {
+        this.setState({
+          pokemon: response
+        })
       })
-    })
   }
 
-  render() {
+  renderSearchBox() {
     return (
-
-      <div>
-
-        <div className="img">
-          <div className="container search-box">
-            <form className="flex">
-              <input className="form-control form-control-lg" placeholder="Qual o Pokémon?" />
-              <button className="btn btn-secondary btn-lg button-search" type="submit">Pesquisar</button>
-            </form>
-          </div>
+      <div className="img">
+        <div className="container search-box">
+          <form className="flex">
+            <input className="form-control form-control-lg" placeholder="Qual o Pokémon?" />
+            <button className="btn btn-secondary btn-lg button-search" type="submit">Pesquisar</button>
+          </form>
         </div>
+      </div>
 
-        <div className="bg-light">
-          <div className="container">
-            <div className="results-box">
+    )
+  }
 
-              {/* Card */}
-              <div className="card text-white bg-card mb-3 cards">{/*style="max-width: 18rem;*/}
-                <div className="card-header">Nome do Pokémon</div>
+  renderResultBox() {
+    return (
+      <div className="bg-light">
+        <div className="container">
+          <div className="results-box">
 
-                <div className="card-body">
-
-                  <h5 className="card-title">Tipos</h5>
-                  <div className="types">
-                    <div className="type-poke">
-                      <p>Fogo</p>
-                    </div>
-                    <div className="type-poke">
-                      <p>Fogo</p>
-                    </div>
+            {/* Card */}
+            <div className="card text-white bg-card mb-3 cards">
+              <div className="card-header nome-poke">Nome do Pokémon</div>
+              <div className="card-body">
+                <h5 className="card-title">Tipos</h5>
+                <div className="types">
+                  <div className="type-poke">
+                    <p>Fogo</p>
                   </div>
+                  <div className="type-poke">
+                    <p>Fogo</p>
+                  </div>
+                </div>
 
-                  <h5 className="card-title">Fraquezas</h5>
-                  <div className="types">
-                    <div className="type-poke">
-                      <p>Terra</p>
-                    </div>
-                    <div className="type-poke">
-                      <p>Agua</p>
-                    </div>
+                <h5 className="card-title">Fraquezas</h5>
+                <div className="types">
+                  <div className="type-poke">
+                    <p>Terra</p>
+                  </div>
+                  <div className="type-poke">
+                    <p>Agua</p>
                   </div>
                 </div>
               </div>
@@ -71,6 +73,20 @@ class SearchBox extends React.Component {
     )
   }
 
+  render() {
+    return (
+
+      <div>
+        {this.renderSearchBox()}
+
+        {!isEmpty(this.state.pokemon)
+          ? this.renderResultBox()
+          : null
+        }
+
+      </div>
+    )
+  }
 }
 
 export default SearchBox
